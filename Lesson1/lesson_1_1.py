717512e8e78e83fc26d51d0f8250f7afabf4e5f6
Анна <an.krivosheeva@gmail.com>
2020-04-10 01:42:26 +0300

    My first Python basics homework

--- Lesson1/lesson_1_1.py
+++ Lesson1/lesson_1_1.py
@@ -1,34 +1,8 @@
-print("hellow world")
-a="2"
-print(a)
-print("hellow world")
-a="2"
-print(a)
-
-print("hellow world")
-a="2"
-print(a)
-print("hellow world")
-a="2"
-print(a)
-
-print("hellow world")
-a="2"
-print(a)
-print("hellow world")
-a="2"
-print(a)
-
-print("hellow world")
-a="2"
-print(a)
-print("hellow world")
-a="2"
-print(a)
-
-print("hellow world")
-a="2"
-print(a)
-print("hellow world")
-a="2"
-print(a)
+name = "Анна"
+age = "30"
+print("Привет, меня зовут", name, "и мне", age, "лет\n")
+name_1 = input("Введите Ваше имя: ")
+age_1 = int(input("Введите Ваш возраст цифрами: "))
+experience = input("Укажите стаж работы: ")
+print("Вас зовут", name_1, "и вам", age_1, "лет,\n", "стаж работы:", experience, "\n")
+print("Приятно познакомиться,", name_1, "!")
--- Lesson1/lesson_1_2.py
+++ Lesson1/lesson_1_2.py
@@ -1,13 +1,5 @@
-rint("hellow world")
-a="2"
-print(a)
-print("hellow world")
-a="2"
-print(a)
-
-print("hellow world")
-a="2"
-print(a)
-print("hellow world")
-a="2"
-print(a)
+sec = int(input("Введите время в секундах: "))
+h = ((sec // 3600)) % 24
+m = (sec // 60) % 60
+s = sec % 60
+print('{0}:{1:=02}:{2:=02}'.format(h, m, s))
