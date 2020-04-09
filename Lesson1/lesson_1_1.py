342f711b2e891f5559d787405a1ac7d75cc4ae51
Анна <an.krivosheeva@gmail.com>
2020-04-09 20:49:03 +0300

    My first Python basics homework

---
+++ Lesson1/lesson_1_1.py
@@ -0,0 +1,20 @@
+print("hellow world")
+a="2"
+print(a)
+print("hellow world")
+a="2"
+print(a)
+
+print("hellow world")
+a="2"
+print(a)
+print("hellow world")
+a="2"
+print(a)
+
+print("hellow world")
+a="2"
+print(a)
+print("hellow world")
+a="2"
+print(a)
