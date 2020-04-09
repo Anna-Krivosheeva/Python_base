042970a452426bd5fe2be294020b001ccc080ce7
Анна <an.krivosheeva@gmail.com>
2020-04-10 01:37:03 +0300

    Merge branch 'master' of D:\Python_base with conflicts.

---
+++ Lesson1/lesson_1_5.py
@@ -0,0 +1,12 @@
+rev = float(input("Введите сумму выручки, руб.: "))
+cost = float(input("Введите сумму издержек, руб.: "))
+prof = rev - cost
+if rev > cost:
+    print("\nВы работаете с прибылью!")
+    efficiency = rev / (rev - cost)
+    print("Рентабельность фирмы: ", "{:.2f}".format(efficiency), "%", sep='')
+    worker = int(input("\nВведите количество сотрудников: "))
+    one_worker = prof / worker
+    print("Прибыль на одного сотрудника составляет, руб.:", "{:.2f}".format(one_worker))
+else:
+    print("\nВы работаете с убытками!")
