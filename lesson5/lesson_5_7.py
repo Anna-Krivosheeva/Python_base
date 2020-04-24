2149482c17d7a972659511dd04055ec40b94639b
Анна <an.krivosheeva@gmail.com>
2020-04-24 11:09:43 +0300

    ready

--- lesson5/lesson_5_7.py
+++ lesson5/lesson_5_7.py
@@ -14,3 +14,29 @@
 Подсказка: использовать менеджер контекста.
 """
 
+import json
+from typing import List, Any
+
+result = []
+profit = {}
+average = []
+
+with open("file_5_7.txt", "r", encoding="utf-8") as file_read:
+    count = 1
+    while True:
+        line = file_read.readline().split()
+        if not line:
+            break
+        profit[line[0]] = float(line[-2]) - float(line[-1])
+        if profit[line[0]] > 0:
+            average.append(profit[line[0]])
+        count += 1
+
+result = [profit, {"average_profit": sum(average) / len(average)}]
+print(f"Результат вычислений:\n {result}")
+
+with open("file_5_7.json", "w", encoding="utf-8") as file_write:
+    json.dump(result, file_write)
+
+print(f"Результат записан в файл {file_write}")
+
